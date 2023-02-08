da0b29a2f63ff295f3c8bfa0f90333e20f3f06c6
Dmytro Tolstoi <dima@Dmytros-Macbook13.local>
2023-02-08 18:00:28 +0100

    added sum for vector

--- main.cpp
+++ main.cpp
@@ -2,7 +2,17 @@
 // Created by Dmytro Tolstoi on 08.02.2023.
 //
 #include <iostream>
+#include <vector>
 using namespace std;
+
+int sum(vector<int> vec)
+{
+    int sum = 0;
+    for_each(vec.begin(),vec.end(),[&sum](int a){
+        sum+=a;
+    });
+    return sum;
+}
 
 int main()
 {
